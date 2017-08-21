bc08dbf1362b0925b8b091434595fe5694abaed8
Mikko Seppänen <alvari83@gmail.com>
2017-08-21 14:40:53 +0300

    Moved time dumping into database package for use in GUI

--- dumper/main.go
+++ dumper/main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
-	"errors"
 	"flag"
 	"log"
 	"os"
 	"time"
 
-	"github.com/boltdb/bolt"
-	"github.com/msepp/stopwatch/model"
 	"github.com/msepp/stopwatch/stopwatchdb"
 )
 
@@ -18,7 +14,6 @@ const dateFmt = "2006-01-02"
 
 var start time.Time
 var end time.Time
-var db *bolt.DB
 var groupID int
 
 func main() {
@@ -49,8 +44,6 @@ func main() {
 
 	if endStr == "" {
 		end = time.Now()
-		end = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local)
-		end = end.AddDate(0, 0, 1)
 
 	} else {
 		if end, err = time.Parse(dateFmt, endStr); err != nil {
@@ -58,109 +51,22 @@ func main() {
 		}
 	}
 
-	if end.Before(start) || end.Equal(start) {
-		log.Fatalf("End is a date after start or same as start.")
-	}
-
 	if st, err := os.Stat(dbPath); err != nil || st.IsDir() {
 		log.Fatalf("Invalid database path")
 	}
 
-	start = start.UTC()
-	end = end.UTC()
-
 	// Open database
-	if db, err = bolt.Open(dbPath, 0600, &bolt.Options{
-		ReadOnly: true,
-	}); err != nil {
-		log.Fatalf("Error opening database: %s", err)
+	db := stopwatchdb.New()
+	if err = db.Open(dbPath); err != nil {
+		log.Fatalf("Opening database failed: %s", err)
 	}
 
-	if err = dump(); err != nil {
+	usage, err := db.GetUsage(groupID, start, end)
+	if err != nil {
 		log.Fatalf("dumping failed: %s", err)
-	}
-}
-
-func dump() error {
-	output := map[string]map[string]model.TaskDuration{}
-
-	tasks := []*model.Task{}
-
-	// open group tasks
-	if err := db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(stopwatchdb.BucketTasks))
-		bg := b.Bucket(stopwatchdb.Itob(groupID))
-		if bg == nil {
-			return errors.New("group not found")
-		}
-
-		return bg.ForEach(func(k []byte, v []byte) error {
-			var t model.Task
-			json.Unmarshal(v, &t)
-			tasks = append(tasks, &t)
-			return nil
-		})
-	}); err != nil {
-		return err
-	}
-
-	// Go through each task, day by day.
-	for _, task := range tasks {
-		if err := db.View(func(tx *bolt.Tx) error {
-			b := tx.Bucket([]byte(stopwatchdb.BucketSlices))
-			bs := b.Bucket(bytes.Join([][]byte{
-				stopwatchdb.Itob(task.GroupID),
-				stopwatchdb.Itob(task.ID)},
-				[]byte("-"),
-			))
-			if bs == nil {
-				log.Printf("Unable to find slices for task %d:%s", task.ID, task.Name)
-				return nil
-			}
-
-			min := []byte(start.Format(time.RFC3339))
-			max := []byte(end.Format(time.RFC3339))
-
-			// Seek to start date
-			c := bs.Cursor()
-			for k, v := c.Seek(min); k != nil && bytes.Compare(k, max) < 0; k, v = c.Next() {
-				starttime, _ := time.Parse(time.RFC3339, string(k))
-				date := starttime.Format(dateFmt)
-
-				if v == nil {
-					continue
-				}
-
-				endtime, _ := time.Parse(time.RFC3339, string(v))
-				if endtime.IsZero() {
-					continue
-				}
-
-				dur := endtime.Sub(starttime)
-
-				log.Printf("%s/%s: %s", task.CostCode, task.Name, endtime)
-
-				if _, ok := output[task.CostCode]; !ok {
-					output[task.CostCode] = map[string]model.TaskDuration{}
-				}
-
-				if _, ok := output[task.CostCode][date]; !ok {
-					output[task.CostCode][date] = model.TaskDuration{}
-				}
-
-				od := output[task.CostCode][date]
-				od.Add(dur)
-				output[task.CostCode][date] = od
-			}
-
-			// Iterate slices until we hit end.
-			return nil
-		}); err != nil {
-			return err
-		}
 	}
 
 	enc := json.NewEncoder(os.Stdout)
 	enc.SetIndent("", "  ")
-	return enc.Encode(output)
+	enc.Encode(usage)
 }
---
+++ stopwatchdb/dump.go
@@ -0,0 +1,133 @@
+package stopwatchdb
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"log"
+	"time"
+
+	"github.com/boltdb/bolt"
+	"github.com/msepp/stopwatch/model"
+)
+
+const dateFmt = "2006-01-02"
+
+// Usage is a time usage report. Contains work for one group.
+type Usage struct {
+	// Daily contains daily usage per cost code.
+	Daily map[string]map[string]model.TaskDuration
+	// Contains total usage per cost code.
+	Total map[string]model.TaskDuration
+}
+
+func (db *StopwatchDB) GetUsage(group int, start, end time.Time) (*Usage, error) {
+	daily := map[string]map[string]model.TaskDuration{}
+	total := map[string]model.TaskDuration{}
+
+	tasks := []*model.Task{}
+
+	// Normalize dates to begin of start date and end of end date.
+	start = time.Date(start.Year(), start.Month(), start.Day(), 0, 0, 0, 0, start.Location())
+	end = time.Date(end.Year(), end.Month(), end.Day()+1, 0, 0, 0, 0, end.Location())
+	end = end.Add(-1 * time.Second)
+
+	// Handle in UTC
+	start = start.UTC()
+	end = end.UTC()
+
+	// Check order
+	if start.Equal(end) || start.After(end) {
+		return nil, errors.New("start must be a time before end.")
+	}
+
+	// open group tasks
+	if err := db.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(BucketTasks))
+		bg := b.Bucket(Itob(group))
+		if bg == nil {
+			return errors.New("group not found")
+		}
+
+		return bg.ForEach(func(k []byte, v []byte) error {
+			var t model.Task
+			json.Unmarshal(v, &t)
+			tasks = append(tasks, &t)
+			return nil
+		})
+	}); err != nil {
+		return nil, err
+	}
+
+	// Minimum and maximum timestamps.
+	min := []byte(start.Format(time.RFC3339))
+	max := []byte(end.Format(time.RFC3339))
+
+	// Generate dates to result.
+
+	// Go through each task, day by day.
+	for _, task := range tasks {
+		if err := db.db.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte(BucketSlices))
+			bs := b.Bucket(bytes.Join([][]byte{
+				Itob(task.GroupID),
+				Itob(task.ID)},
+				[]byte("-"),
+			))
+			if bs == nil {
+				log.Printf("Unable to find slices for task %d:%s", task.ID, task.Name)
+				return nil
+			}
+
+			// Seek to start date
+			c := bs.Cursor()
+			for k, v := c.Seek(min); k != nil && bytes.Compare(k, max) < 0; k, v = c.Next() {
+				starttime, _ := time.Parse(time.RFC3339, string(k))
+				date := starttime.Format(dateFmt)
+
+				if v == nil {
+					continue
+				}
+
+				endtime, _ := time.Parse(time.RFC3339, string(v))
+				if endtime.IsZero() {
+					continue
+				}
+
+				dur := endtime.Sub(starttime)
+
+				log.Printf("%s/%s: %s", task.CostCode, task.Name, endtime)
+
+				if _, ok := daily[task.CostCode]; !ok {
+					daily[task.CostCode] = map[string]model.TaskDuration{}
+				}
+
+				if _, ok := daily[task.CostCode][date]; !ok {
+					daily[task.CostCode][date] = model.TaskDuration{}
+				}
+
+				if _, ok := total[task.CostCode]; !ok {
+					total[task.CostCode] = model.TaskDuration{}
+				}
+
+				od := daily[task.CostCode][date]
+				od.Add(dur)
+				daily[task.CostCode][date] = od
+
+				od = total[task.CostCode]
+				od.Add(dur)
+				total[task.CostCode] = od
+			}
+
+			// Iterate slices until we hit end.
+			return nil
+		}); err != nil {
+			return nil, err
+		}
+	}
+
+	return &Usage{
+		Daily: daily,
+		Total: total,
+	}, nil
+}
