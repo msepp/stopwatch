dc2e41497ed1aca0edc678037415bcc372ce065d
Mikko Seppänen <alvari83@gmail.com>
2017-08-22 10:58:24 +0300

    Added tools for retroactively fixing slices into the dumper tool

--- dumper/main.go
+++ dumper/main.go
@@ -15,17 +15,21 @@ const dateFmt = "2006-01-02"
 var start time.Time
 var end time.Time
 var groupID int
+var taskID int
 
 func main() {
 	var dbPath string
 	var startStr string
 	var endStr string
+	var dumpType string
 	var err error
 
 	flag.StringVar(&dbPath, "db", "data.dat", "path to database")
-	flag.StringVar(&startStr, "start", "", "start date YYYY-MM-DD. Defaults to start of current day.")
-	flag.StringVar(&endStr, "end", "", "end date YYYY-MMM-DD. Defaults to now")
-	flag.IntVar(&groupID, "groupID", 0, "Group ID to dump")
+	flag.StringVar(&startStr, "start", "", "start date (YYYY-MM-DD for reports, RFC 3339 for slices). Defaults to start of current day for reports.")
+	flag.StringVar(&endStr, "end", "", "end date (YYYY-MM-DD for reports, RFC 3339 for slices). Defaults to now for reports.")
+	flag.IntVar(&groupID, "groupID", 0, "Group ID to dump/modify")
+	flag.IntVar(&taskID, "taskID", 0, "Task ID to dump/modify")
+	flag.StringVar(&dumpType, "type", "report", "operation type. 'slices' returns recorded slices, 'report' gives a nice report, 'setslice' allows setting a slice and 'rmslice' removes slice.")
 	flag.Parse()
 
 	if groupID <= 0 {
@@ -34,20 +38,34 @@ func main() {
 
 	now := time.Now()
 
-	if startStr == "" {
-		start = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local)
-	} else {
-		if start, err = time.Parse(dateFmt, startStr); err != nil {
-			log.Fatalf("Invalid start date: %s", err)
+	switch dumpType {
+	case "setslice", "rmslice":
+		if start, err = time.Parse(time.RFC3339, startStr); err != nil {
+			log.Fatalf("Invalid start datetime: %s", err)
 		}
-	}
 
-	if endStr == "" {
-		end = time.Now()
+		if dumpType != "rmslice" {
+			if end, err = time.Parse(time.RFC3339, endStr); err != nil {
+				log.Fatalf("Invalid end datetime: %s", err)
+			}
+		}
 
-	} else {
-		if end, err = time.Parse(dateFmt, endStr); err != nil {
-			log.Fatalf("Invalid end date: %s", err)
+	default:
+		if startStr == "" {
+			start = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local)
+		} else {
+			if start, err = time.Parse(dateFmt, startStr); err != nil {
+				log.Fatalf("Invalid start date: %s", err)
+			}
+		}
+
+		if endStr == "" {
+			end = time.Now()
+
+		} else {
+			if end, err = time.Parse(dateFmt, endStr); err != nil {
+				log.Fatalf("Invalid end date: %s", err)
+			}
 		}
 	}
 
@@ -61,12 +79,37 @@ func main() {
 		log.Fatalf("Opening database failed: %s", err)
 	}
 
-	usage, err := db.GetUsage(groupID, start, end)
-	if err != nil {
-		log.Fatalf("dumping failed: %s", err)
+	var result interface{}
+	switch dumpType {
+	case "report":
+		result, err = db.GetUsage(groupID, start, end)
+		if err != nil {
+			log.Fatalf("dumping failed: %s", err)
+		}
+
+	case "slices":
+		result, err = db.GetSlices(groupID, start, end)
+		if err != nil {
+			log.Fatalf("dumping failed: %s", err)
+		}
+
+	case "setslice":
+		result, err = db.SetSlice(groupID, taskID, start, end)
+		if err != nil {
+			log.Fatalf("set slice failed: %s", err)
+		}
+
+	case "rmslice":
+		result, err = db.RemoveSlice(groupID, taskID, start)
+		if err != nil {
+			log.Fatalf("removing slice failed: %s", err)
+		}
+
+	default:
+		log.Fatalf("Invalid dump type")
 	}
 
 	enc := json.NewEncoder(os.Stdout)
 	enc.SetIndent("", "  ")
-	enc.Encode(usage)
+	enc.Encode(result)
 }
--- stopwatchdb/dump.go
+++ stopwatchdb/dump.go
@@ -28,7 +28,7 @@ type CostUsage struct {
 
 // UsageReport is a time usage report. Contains work for one group.
 type UsageReport struct {
-	// Array with the dates in the report
+	// Dates is an array with the dates in the report
 	Dates []Usage
 	// CostCodes contains the time used per cost code
 	CostCodes []CostUsage
@@ -36,6 +36,202 @@ type UsageReport struct {
 	Combined model.TaskDuration
 }
 
+// TaskSlices reports a single tasks slices
+type TaskSlices struct {
+	// Task name
+	Name string
+	// ID is task ID
+	ID int
+	// Slices are tasks slices
+	Slices []Slice
+}
+
+// Slice documents a single period of work
+type Slice struct {
+	Start time.Time
+	End   time.Time
+}
+
+// SetSlice sets a slice for a task in a group and updates time used for the
+// task. Overwrites if a slice exists with the given start time.
+// Returns updated task on success
+func (db *StopwatchDB) SetSlice(groupID, taskID int, start, end time.Time) (*model.Task, error) {
+	var err error
+	var oldDuration time.Duration
+	var t *model.Task
+
+	if start.After(end) {
+		return nil, errors.New("start must be before end")
+	}
+
+	// get task
+	if t, err = db.GetTask(groupID, taskID); err != nil {
+		return nil, err
+	}
+
+	start = start.UTC()
+	end = end.UTC()
+
+	err = db.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(BucketSlices))
+		bs := b.Bucket(bytes.Join([][]byte{
+			Itob(groupID),
+			Itob(taskID)},
+			[]byte("-"),
+		))
+
+		// Get existing value first.
+		buf := bs.Get([]byte(start.Format(time.RFC3339)))
+		if buf != nil {
+			oldEnd, _ := time.Parse(time.RFC3339, string(buf))
+			oldDuration = oldEnd.Sub(start)
+		}
+
+		return bs.Put([]byte(start.Format(time.RFC3339)), []byte(end.Format(time.RFC3339)))
+	})
+
+	// Update task time used.
+	t.Used.Duration = t.Used.Duration - oldDuration
+	t.Used.Add(end.Sub(start))
+
+	if err = db.SaveTask(t); err != nil {
+		return nil, err
+	}
+
+	return t, nil
+}
+
+// RemoveSlice deletes a slice from task. Task time used is updated to reflect
+// the change. Returns changed Task on success.
+func (db *StopwatchDB) RemoveSlice(groupID, taskID int, start time.Time) (*model.Task, error) {
+	var err error
+	var oldDuration time.Duration
+	var t *model.Task
+
+	// get task
+	if t, err = db.GetTask(groupID, taskID); err != nil {
+		return nil, err
+	}
+
+	start = start.UTC()
+	err = db.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(BucketSlices))
+		bs := b.Bucket(bytes.Join([][]byte{
+			Itob(groupID),
+			Itob(taskID)},
+			[]byte("-"),
+		))
+
+		// Get existing value first.
+		buf := bs.Get([]byte(start.Format(time.RFC3339)))
+		if buf != nil {
+			oldEnd, _ := time.Parse(time.RFC3339, string(buf))
+			oldDuration = oldEnd.Sub(start)
+		} else {
+			return errors.New("slice not found")
+		}
+
+		return bs.Delete([]byte(start.Format(time.RFC3339)))
+	})
+
+	// Update task time used.
+	t.Used.Duration = t.Used.Duration - oldDuration
+	if err = db.SaveTask(t); err != nil {
+		return nil, err
+	}
+
+	return t, nil
+}
+
+// GetSlices returns a report of slices recorded for a group between given start
+// and end times.
+func (db *StopwatchDB) GetSlices(group int, start, end time.Time) ([]TaskSlices, error) {
+	slices := []TaskSlices{}
+	tasks := []*model.Task{}
+
+	// Normalize dates to begin of start date and end of end date.
+	start = time.Date(start.Year(), start.Month(), start.Day(), 0, 0, 0, 0, start.Location())
+	end = time.Date(end.Year(), end.Month(), end.Day()+1, 0, 0, 0, 0, end.Location())
+	end = end.Add(-1 * time.Second)
+
+	// Handle in UTC
+	start = start.UTC()
+	end = end.UTC()
+
+	// Check order
+	if start.Equal(end) || start.After(end) {
+		return nil, errors.New("start must be a time before end.")
+	}
+
+	// open group tasks
+	if err := db.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(BucketTasks))
+		bg := b.Bucket(Itob(group))
+		if bg == nil {
+			return errors.New("group not found")
+		}
+
+		return bg.ForEach(func(k []byte, v []byte) error {
+			var t model.Task
+			json.Unmarshal(v, &t)
+			tasks = append(tasks, &t)
+			return nil
+		})
+	}); err != nil {
+		return nil, err
+	}
+
+	// Minimum and maximum timestamps.
+	min := []byte(start.Format(time.RFC3339))
+	max := []byte(end.Format(time.RFC3339))
+
+	// Go through each task, day by day.
+	for _, task := range tasks {
+		if err := db.db.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte(BucketSlices))
+			bs := b.Bucket(bytes.Join([][]byte{
+				Itob(task.GroupID),
+				Itob(task.ID)},
+				[]byte("-"),
+			))
+			if bs == nil {
+				log.Printf("Unable to find slices for task %d:%s", task.ID, task.Name)
+				return nil
+			}
+
+			ts := TaskSlices{Name: task.Name, ID: task.ID, Slices: []Slice{}}
+
+			// Seek to start date
+			c := bs.Cursor()
+			for k, v := c.Seek(min); k != nil && bytes.Compare(k, max) < 0; k, v = c.Next() {
+				if v == nil {
+					continue
+				}
+
+				s, _ := time.Parse(time.RFC3339, string(k))
+				e, _ := time.Parse(time.RFC3339, string(v))
+				if e.IsZero() {
+					continue
+				}
+
+				ts.Slices = append(ts.Slices, Slice{Start: s, End: e})
+			}
+
+			if len(ts.Slices) > 0 {
+				slices = append(slices, ts)
+			}
+
+			// Iterate slices until we hit end.
+			return nil
+		}); err != nil {
+			return nil, err
+		}
+	}
+
+	return slices, nil
+}
+
+// GetUsage returns a report of time used for a group during given period of time.
 func (db *StopwatchDB) GetUsage(group int, start, end time.Time) (*UsageReport, error) {
 	daily := map[string]map[string]model.TaskDuration{}
 	total := map[string]model.TaskDuration{}
@@ -103,18 +299,17 @@ func (db *StopwatchDB) GetUsage(group int, start, end time.Time) (*UsageReport, 
 			// Seek to start date
 			c := bs.Cursor()
 			for k, v := c.Seek(min); k != nil && bytes.Compare(k, max) < 0; k, v = c.Next() {
-				starttime, _ := time.Parse(time.RFC3339, string(k))
-				date := starttime.Format(dateFmt)
-
 				if v == nil {
 					continue
 				}
 
+				starttime, _ := time.Parse(time.RFC3339, string(k))
 				endtime, _ := time.Parse(time.RFC3339, string(v))
 				if endtime.IsZero() {
 					continue
 				}
 
+				date := starttime.Format(dateFmt)
 				dur := endtime.Sub(starttime)
 
 				log.Printf("%s/%s: %s", task.CostCode, task.Name, endtime)
