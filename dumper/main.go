5d1ccb2cc7a7e63fe7d0922bffe487c07c11a12c
Mikko Seppänen <alvari83@gmail.com>
2017-08-23 14:05:47 +0300

    Cleaned up dumper a little bit

--- dumper/main.go
+++ dumper/main.go
@@ -24,6 +24,7 @@ func main() {
 	var dumpType string
 	var err error
 
+	// Define and parse supported command line flags.
 	flag.StringVar(&dbPath, "db", "data.dat", "path to database")
 	flag.StringVar(&startStr, "start", "", "start date (YYYY-MM-DD for reports, RFC 3339 for slices). Defaults to start of current day for reports.")
 	flag.StringVar(&endStr, "end", "", "end date (YYYY-MM-DD for reports, RFC 3339 for slices). Defaults to now for reports.")
@@ -32,18 +33,27 @@ func main() {
 	flag.StringVar(&dumpType, "type", "report", "operation type. 'slices' returns recorded slices, 'report' gives a nice report, 'setslice' allows setting a slice and 'rmslice' removes slice.")
 	flag.Parse()
 
+	// We require a group ID for all operations
 	if groupID <= 0 {
 		log.Fatalf("groupID needs to be a positive non-zero integer")
 	}
 
+	// Current time for convenience.
 	now := time.Now()
 
+	// Check rest of the command-line based on operation type
 	switch dumpType {
 	case "setslice", "rmslice":
+		// slice operations require a task ID
+		if taskID <= 0 {
+			log.Fatalf("taskID needs to be a positive non-zero integer")
+		}
+
 		if start, err = time.Parse(time.RFC3339, startStr); err != nil {
 			log.Fatalf("Invalid start datetime: %s", err)
 		}
 
+		// Only parse end datetime if not removing a slice. rmslice doesn't need it.
 		if dumpType != "rmslice" {
 			if end, err = time.Parse(time.RFC3339, endStr); err != nil {
 				log.Fatalf("Invalid end datetime: %s", err)
@@ -51,6 +61,7 @@ func main() {
 		}
 
 	default:
+		// Other operations take a date only format. These also accept defaults.
 		if startStr == "" {
 			start = time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.Local)
 		} else {
@@ -69,11 +80,12 @@ func main() {
 		}
 	}
 
+	// Check that the database file exists first. This is to prevent the db lib
+	// from creating an empty database if non-existing path was given.
 	if st, err := os.Stat(dbPath); err != nil || st.IsDir() {
 		log.Fatalf("Invalid database path")
 	}
 
-	// Open database
 	db := stopwatchdb.New()
 	if err = db.Open(dbPath); err != nil {
 		log.Fatalf("Opening database failed: %s", err)
@@ -83,30 +95,22 @@ func main() {
 	switch dumpType {
 	case "report":
 		result, err = db.GetUsage(groupID, start, end)
-		if err != nil {
-			log.Fatalf("dumping failed: %s", err)
-		}
 
 	case "slices":
 		result, err = db.GetSlices(groupID, start, end)
-		if err != nil {
-			log.Fatalf("dumping failed: %s", err)
-		}
 
 	case "setslice":
 		result, err = db.SetSlice(groupID, taskID, start, end)
-		if err != nil {
-			log.Fatalf("set slice failed: %s", err)
-		}
 
 	case "rmslice":
 		result, err = db.RemoveSlice(groupID, taskID, start)
-		if err != nil {
-			log.Fatalf("removing slice failed: %s", err)
-		}
 
 	default:
 		log.Fatalf("Invalid dump type")
+	}
+
+	if err != nil {
+		log.Fatalf("Operation '%s' failed: %s", dumpType, err)
 	}
 
 	enc := json.NewEncoder(os.Stdout)
